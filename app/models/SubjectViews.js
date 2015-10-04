92d4a74086688c99c18446248c51b1e5aed1384d
Prince <leewaygroups@gmail.com>
2015-10-05 01:01:00 +0600

    fix

--- app/models/SubjectViews.js
+++ app/models/SubjectViews.js
@@ -1,3 +1,8 @@
+var mongoose = require('mongoose');
+var db = require('./config/db');
+
+// connect to our mongoDB database (commented out after you enter in your own credentials)
+var connectionsubject = mongoose.createConnection(db.urlSubjectViews);
 
 // define our nerd model
 // module.exports allows us to pass this to other files when it is called
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // modules =================================================
 var express        = require('express');
 var app            = express();
-var mongoose       = require('mongoose');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 
@@ -9,11 +8,6 @@ var methodOverride = require('method-override');
 	
 // config files
 var port = process.env.PORT || 8080; // set our port
-var db = require('./config/db');
-
-// connect to our mongoDB database (commented out after you enter in your own credentials)
-connectionsubject = mongoose.createConnection(db.urlSubjectViews);
-
 
 
 // get all data/stuff of the body (POST) parameters
