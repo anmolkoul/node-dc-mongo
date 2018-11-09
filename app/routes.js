58b3fc88c5fcabfb33d795ab2e0a8c1b636692ae
rayyuan90 <rayyuan90@gmail.com>
2018-11-09 15:47:43 +0800

    add find condition and limit to 100

--- app/routes.js
+++ app/routes.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
 	// sample api route
  app.get('/api/data', function(req, res) {
   // use mongoose to get all nerds in the database
-  collection.findOne({}, function(err, subjectDetails) {
+  collection.find({},{'_id': 0, 'school_state': 1, 'resource_type': 1, 'poverty_level': 1, 'date_posted': 1, 'total_donations': 1, 'funding_status': 1, 'grade_level': 1}).limit(100).toArray(function(err, subjectDetails) {
    // if there is an error retrieving, send the error.
        // nothing after res.send(err) will execute
    if (err)
